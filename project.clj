4e95c582ff1d9affb23a3a07b2d22df63287a946
trikitrok <trikitrok@gmail.com>
2015-04-18 00:06:11 +0200

    Gilded Rose original code

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject gilded-rose "0.0.1-SNAPSHOT"
+  :description "Gilded Rose kata in Clojure"
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
+  
