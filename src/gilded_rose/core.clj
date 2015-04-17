dfc75ba19204006f1bcc47fd0c0575bdecee7920
trikitrok <trikitrok@gmail.com>
2015-04-18 00:15:00 +0200

    Added tests and fixed several mistakes related with quality not being between 0 and 50

--- src/gilded_rose/core.clj
+++ src/gilded_rose/core.clj
@@ -2,42 +2,43 @@
 
 (defn update-quality [items]
   (map
-    (fn[item] (cond
-      (and (< (:sell-in item) 0) (= "Backstage passes to a TAFKAL80ETC concert" (:name item)))
+    (fn[item] 
+      (cond
+        (and (< (:sell-in item) 0) (= "Backstage passes to a TAFKAL80ETC concert" (:name item)))
         (merge item {:quality 0})
-      (or (= (:name item) "Aged Brie") (= (:name item) "Backstage passes to a TAFKAL80ETC concert"))
+        (or (= (:name item) "Aged Brie") (= (:name item) "Backstage passes to a TAFKAL80ETC concert"))
         (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 5) (< (:sell-in item) 10))
-          (merge item {:quality (inc (inc (:quality item)))})
+          (merge item {:quality (min 50 (inc (inc (:quality item))))})
           (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 0) (< (:sell-in item) 5))
-            (merge item {:quality (inc (inc (inc (:quality item))))})
+            (merge item {:quality (min 50 (inc (inc (inc (:quality item)))))})
             (if (< (:quality item) 50)
               (merge item {:quality (inc (:quality item))})
               item)))
-      (< (:sell-in item) 0)
+        (< (:sell-in item) 0)
         (if (= "Backstage passes to a TAFKAL80ETC concert" (:name item))
           (merge item {:quality 0})
           (if (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
-            (merge item {:quality (- (:quality item) 2)})
+            (merge item {:quality (max 0 (- (:quality item) 2))})
             item))
-      (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
-        (merge item {:quality (dec (:quality item))})
-      :else item))
-  (map (fn [item]
-      (if (not= "Sulfuras, Hand of Ragnaros" (:name item))
-        (merge item {:sell-in (dec (:sell-in item))})
-        item))
-  items)))
+        (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
+        (merge item {:quality (max 0 (dec (:quality item)))})
+        :else item))
+    (map (fn [item]
+           (if (not= "Sulfuras, Hand of Ragnaros" (:name item))
+             (merge item {:sell-in (dec (:sell-in item))})
+             item))
+         items)))
 
 (defn item [item-name, sell-in, quality]
   {:name item-name, :sell-in sell-in, :quality quality})
 
 (defn update-current-inventory[]
   (let [inventory 
-    [
-      (item "+5 Dexterity Vest" 10 20)
-      (item "Aged Brie" 2 0)
-      (item "Elixir of the Mongoose" 5 7)
-      (item "Sulfuras, Hand of Ragnaros" 0 80)
-      (item "Backstage passes to a TAFKAL80ETC concert" 15 20)
-    ]]
+        [
+         (item "+5 Dexterity Vest" 10 20)
+         (item "Aged Brie" 2 0)
+         (item "Elixir of the Mongoose" 5 7)
+         (item "Sulfuras, Hand of Ragnaros" 0 80)
+         (item "Backstage passes to a TAFKAL80ETC concert" 15 20)
+         ]]
     (update-quality inventory)))
---
+++ test/gilded_rose/core_test.clj
@@ -0,0 +1,106 @@
+(ns gilded-rose.core-test
+  (:use midje.sweet)
+  (:use [gilded-rose.core]))
+
+(defn pass-days [n inventory]
+  (nth (iterate update-quality inventory) n))
+
+(facts 
+  "about gilded rose"
+  
+  (facts 
+    "Sulfuras"
+    (pass-days 
+      5 
+      [(item "Sulfuras, Hand of Ragnaros" 0 80)]) => [(item "Sulfuras, Hand of Ragnaros" 0 80)])
+  
+  (facts
+    "Regular items"
+    
+    (facts
+      "Quality decreases one by one before selling date"
+      (pass-days 
+        5
+        [(item "+5 Dexterity Vest" 10 20)]) => [(item "+5 Dexterity Vest" 5 15)]
+      
+      (pass-days 
+        5
+        [(item "Elixir of the Mongoose" 5 7)]) => [(item "Elixir of the Mongoose" 0 2)])
+    
+    (fact
+      "Once the sell by date has passed, quality degrades twice as fast"
+      
+      (pass-days 
+        10
+        [(item "+5 Dexterity Vest" 0 20)]) => [(item "+5 Dexterity Vest" -10 0)])
+    
+    (fact
+      "The quality of an item is never negative"
+      
+      (pass-days 
+        20
+        [(item "+5 Dexterity Vest" 10 20)]) => [(item "+5 Dexterity Vest" -10 0)]))
+  
+  (facts 
+    "Aged Brie"
+    
+    (fact
+      "Quality increases by one before sell date"  
+      (pass-days 
+        2
+        [(item "Aged Brie" 2 0)]) => [(item "Aged Brie" 0 2)])
+    
+    (fact
+      "Quality also increases by one after sell date"  
+      (pass-days 
+        4
+        [(item "Aged Brie" 2 0)]) => [(item "Aged Brie" -2 4)])
+    
+    (fact
+      "Quality can't be greater than 50"  
+      (pass-days 
+        51
+        [(item "Aged Brie" 2 0)]) => [(item "Aged Brie" -49 50)]))
+  
+  (facts 
+    "Backstage passages"
+    
+    (fact 
+      "Before 10 days of the concert quality increases one by one"
+      (pass-days 
+        5
+        [(item "Backstage passes to a TAFKAL80ETC concert" 15 20)]) 
+      => [(item "Backstage passes to a TAFKAL80ETC concert" 10 25)])
+    
+    (fact 
+      "Between 10 and 5 days of the concert quality increases two by two"
+      (pass-days 
+        5
+        [(item "Backstage passes to a TAFKAL80ETC concert" 10 20)]) 
+      => [(item "Backstage passes to a TAFKAL80ETC concert" 5 30)])
+    
+    (fact 
+      "The last 5 days before the concert quality increases three by three"
+      (pass-days 
+        5
+        [(item "Backstage passes to a TAFKAL80ETC concert" 5 20)]) 
+      => [(item "Backstage passes to a TAFKAL80ETC concert" 0 35)])
+    
+    (fact 
+      "After the concert quality is 0"
+      (pass-days 
+        6
+        [(item "Backstage passes to a TAFKAL80ETC concert" 5 20)]) 
+      => [(item "Backstage passes to a TAFKAL80ETC concert" -1 0)])
+    
+     (fact 
+      "Quality can't be grater than 50"
+      (pass-days 
+        2
+        [(item "Backstage passes to a TAFKAL80ETC concert" 10 48)]) 
+      => [(item "Backstage passes to a TAFKAL80ETC concert" 8 50)]
+      
+      (pass-days 
+        17
+        [(item "Backstage passes to a TAFKAL80ETC concert" 17 20)]) 
+      => [(item "Backstage passes to a TAFKAL80ETC concert" 0 50)])))
